568f81151e25977396994e70f9460615dacf93ee
Jodie Mullins <jmullins2460@gmail.com>
2025-03-13 12:52:28 -0400

    utf8 encoding issue from csv method and function breakdown due to errors (db creates but no information inserted into database)

--- ForDelDatabase.py
+++ ForDelDatabase.py
@@ -1,9 +1,10 @@
 import sqlite3 as db
 import pandas as pd
 
-def CreateForDel():
 
-    """
+#def CreateForDel():
+
+"""
 
     Establishes foundation for Fordel store's database. 
     Executes 3 tables: 
@@ -11,30 +12,39 @@ def CreateForDel():
                 2) Returns 
                 3) Sales representatives and regions
     
-    """
+"""
     # establish connection to SQL database
-    con = db.connect('Fordel.db')
+con = db.connect('Fordel.db')
 
     #mc for mouseclick
-    mc = con.cursor()
+mc = con.cursor()
 
     # ORDERS
    
-    orders = pd.read_csv('Orders.csv', index_col=None)
+orders = pd.read_csv('Orders.csv', index_col=None)
 
-    orders_table = pd.orders.to_sql(name='orders', con=mc, if_exists='replace')
+orders_table = pd.orders.to_sql(name=orders, con=mc, if_exists='replace')
 
     # RETURNS
 
-    returns = pd.read_csv('Returns.csv', index_col=None)
+returns = pd.read_csv('Returns.csv', index_col=None)
 
-    returns_table = pd.returns.to_sql(name='returns', con=mc, if_exists='replace')
+returns_table = pd.returns.to_sql(name=returns, con=mc, if_exists='replace')
 
     # PEOPLE
 
-    people = pd.read_csv('People.csv', index_col=None)
+people = pd.read_csv('People.csv', index_col=None)
 
-    people_table = pd.people.to_sql(name='people', con=mc, if_exists='replace')
+people_table = pd.people.to_sql(name=people, con=mc, if_exists='replace')
 
     # save changes from INSERT
-    con.commit()
+con.commit()
+
+"""
+    def main():
+        CreateForDel()
+
+if __name__ == "__main__":
+    main()
+
+"""
